fba20f9963133fef607eced6451bc38a57abc84a
Zach Cheney <ztcheney@uark.edu>
2024-08-28 18:06:55 -0500

    Add initial Python script

---
+++ Hello World.py
@@ -0,0 +1,8 @@
+
+num1 = float(input("Enter the firsy number: "))
+num2= float(input("ENter the second number: "))
+
+result = num1 * num2
+
+print ("The product of", num1, "and", num2, "is:", result)
+
